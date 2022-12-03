bea464076f2cacde0a86390c730b0555946d895c
John Doe <shvedok1@mail.ru>
2022-12-03 15:51:49 +0300

    усё

---
+++ Задача 2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number1=int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2=int.Parse(Console.ReadLine());
+            if (number1 > number2)
+            {
+                Console.WriteLine("Первое число " + number1 + " больше чем второе " + number2);
+            }
+            else
+            {
+                Console.WriteLine("Второе число " + number2 + " больше чем первое " + number1);
+            };
